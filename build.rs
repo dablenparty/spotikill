aa9ae10eb000173d90af31816328fab25883015a
dablenparty <hdavenport8902@gmail.com>
2024-01-07 18:12:34 -0500

    Disallowed compilation on non-Windows platforms

--- build.rs
+++ build.rs
@@ -1,5 +1,11 @@
+#![cfg(windows)]
 fn main() {
     windres::Build::new()
         .compile("tray-props.rc")
         .expect("Failed to compile tray-props.rc");
+}
+
+#[cfg(not(windows))]
+fn main() {
+    compile_error!("Currently, this program only works on Windows. Sorry!")
 }
