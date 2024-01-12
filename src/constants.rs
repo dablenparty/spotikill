81b567b76e8dac8bfac993104146d5dee7cedf0c
dablenparty <hdavenport8902@gmail.com>
2024-01-11 23:29:19 -0500

    Add CARGO_MANIFEST_DIR const

--- src/constants.rs
+++ src/constants.rs
@@ -1,12 +1,17 @@
-use const_format::{concatcp, formatcp};
-
+#[cfg(debug_assertions)]
+use const_format::concatcp;
+use const_format::formatcp;
+pub const CARGO_MANIFEST_DIR: &str = env!("CARGO_MANIFEST_DIR");
+#[cfg(debug_assertions)]
+pub const CARGO_PKG_NAME: &str = concatcp!(env!("CARGO_PKG_NAME"), "-debug");
+#[cfg(not(debug_assertions))]
+pub const CARGO_PKG_NAME: &str = env!("CARGO_PKG_NAME");
+#[cfg(debug_assertions)]
+pub const CARGO_PKG_VERSION: &str = concatcp!(env!("CARGO_PKG_VERSION"), "-debug");
+#[cfg(not(debug_assertions))]
+pub const CARGO_PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
 pub const ICON_PATH: &str = formatcp!(
     "{}{}app-icon.ico",
     env!("CARGO_MANIFEST_DIR"),
     std::path::MAIN_SEPARATOR
 );
-#[cfg(debug_assertions)]
-pub const CARGO_PKG_NAME: &str = concatcp!(env!("CARGO_PKG_NAME"), "-debug");
-#[cfg(not(debug_assertions))]
-pub const CARGO_PKG_NAME: &str = env!("CARGO_PKG_NAME");
-pub const CARGO_PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
