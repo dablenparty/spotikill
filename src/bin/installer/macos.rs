cfe5fcd0aa12d4c33b99fcda0b776bc6f471e51b
dablenparty <hdavenport8902@gmail.com>
2024-01-17 21:07:44 -0500

    Create package name constant
    My mac is about to die and I wanna save my changes

--- src/bin/installer/macos.rs
+++ src/bin/installer/macos.rs
@@ -1,3 +1,9 @@
+#![cfg(target_os = "macos")]
+
+use const_format::concatcp;
+use spotikill::constants::CARGO_PKG_NAME;
+
 pub fn install() -> anyhow::Result<()> {
-    todo!("macOS installer is not yet implemented")
+    const PACKAGE_NAME: &str = concatcp!(CARGO_PKG_NAME, ".app");
+    todo!("Implement macOS installer.")
 }
