ffa5471a5187317349e278379a58bdfc14b440c6
dablenparty <hdavenport8902@gmail.com>
2024-01-19 14:33:56 -0500

    Create macOS resources

--- src/constants.rs
+++ src/constants.rs
@@ -11,7 +11,9 @@ pub const CARGO_PKG_VERSION: &str = concatcp!(env!("CARGO_PKG_VERSION"), "-debug
 #[cfg(not(debug_assertions))]
 pub const CARGO_PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
 pub const ICON_PATH: &str = formatcp!(
-    "{}{}app-icon.ico",
+    "{}{}{}{}app-icon.ico",
     env!("CARGO_MANIFEST_DIR"),
+    std::path::MAIN_SEPARATOR,
+    "resources",
     std::path::MAIN_SEPARATOR
 );
